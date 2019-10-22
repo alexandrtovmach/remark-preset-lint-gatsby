7ad71a6affc16d0f6888845f2c84d6b4bf6a5298
alexandrtovmach <alexandrtovmach@gmail.com>
2019-10-22 19:43:47 +0300

    fix: Updated regex for gatsby word validation

--- src/words.js
+++ src/words.js
@@ -2,7 +2,7 @@ module.exports = [
 	{ no: "[Gg]ithub", yes: "GitHub" },
 	{ no: "[Jj]avascript", yes: "JavaScript" },
 	{ no: "[Gg]atsby\\.\?[Jj][Ss]", yes: "Gatsby" },
-	{ no: "gatsby", yes: "Gatsby" },
+	{ no: "(?<!-)gatsby(?!-)", yes: "Gatsby" },
 	{ no: "[Rr]eact\\.\?[Jj][Ss]", yes: "React" },
 	{ no: "node\\.\?[Jj][Ss]", yes: "Node.js" },
 	{ no: "Node[Jj][Ss]", yes: "Node.js" },
