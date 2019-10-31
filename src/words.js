f1c7d97149743ad2b3173f89fa01cc166d6d845f
Michael <184316+muescha@users.noreply.github.com>
2019-10-31 05:27:46 +0100

    languages: add some more possibilities

--- src/words.js
+++ src/words.js
@@ -3,9 +3,11 @@ module.exports = [
 	{ no: "[Jj]avascript", yes: "JavaScript" },
 	{ no: "[Gg]atsby\\.\?[Jj][Ss]", yes: "Gatsby" },
 	{ no: "gatsby", yes: "Gatsby" },
+	{ no: "[Gg]astby", yes: "Gatsby" },
 	{ no: "[Rr]eact\\.\?[Jj][Ss]", yes: "React" },
+	{ no: "react", yes: "React" },
 	{ no: "node\\.\?[Jj][Ss]", yes: "Node.js" },
-	{ no: "Node[Jj][Ss]", yes: "Node.js" },
+	{ no: "[Nn]ode[Jj][Ss]", yes: "Node.js" },
 	{ no: "[Nn]pm\\.\?[Jj][Ss]", yes: "npm" },
 	{ no: "Npm", yes: "npm" },
 ]
